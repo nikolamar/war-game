2d7ff1928bd864d83fe7a367df91c46235cab25a
Nikola Marjanovic <nikolamarjanovic@Nikolas-MBP.home>
2017-03-09 12:54:25 +0100

    Cleaning...

--- src/lib/defaults.js
+++ src/lib/defaults.js
@@ -2,12 +2,12 @@ import CardsJSON from './cards.json';
 
 export const allCards = () => CardsJSON.map(card => ({ 
   id: card,
-  image: `../assets/${card}.png`,
+  image: require(`../assets/${card}.png`),
 }));
 
 export const allPlayers = () => ([
-  { name: 'Player', image: '../assets/PlayerOff.png', score: 0 },
-  { name: 'Comp1', image: '../assets/Comp1Off.png', score: 0 },
-  { name: 'Comp2', image: '../assets/Comp2Off.png', score: 0 },
-  { name: 'Comp3', image: '../assets/Comp3Off.png', score: 0 },
+  { name: 'Player', image: require('../assets/PlayerOff.png'), score: 0 },
+  { name: 'Comp1', image: require('../assets/Comp1Off.png'), score: 0 },
+  { name: 'Comp2', image: require('../assets/Comp2Off.png'), score: 0 },
+  { name: 'Comp3', image: require('../assets/Comp3Off.png'), score: 0 },
 ]);
